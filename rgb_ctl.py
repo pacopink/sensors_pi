5f65f64b8368978844e7567c3e0d9be8e8c3239c
pacopink <13602887904@163.com>
2019-04-22 11:29:35 +0800

    optimize rgb_ctl.py

--- rgb_ctl.py
+++ rgb_ctl.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python
 #coding:utf8
 # Paco Li: 2019-04-22
-# Use REDIS to control PWM LEDS 
-# tested on Raspberry Pi
+# Use REDIS to control PWM LEDS freq and duty_cycle
+# tested on Raspberry Pi import time
+import time, traceback
 
-import time
-import traceback
 import RPi.GPIO as GPIO
-from gpio.sakshat import SAKSHAT
-from gpio import * 
-from gpio.beeper import Beeper 
-import redis
-
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
+import redis
 # redis parameters
 HOST='localhost'
 PORT=6379
@@ -24,7 +19,8 @@ FREQ = 200.0 #Hz
 DUTY_CYCLE = 100.0 #Percentage
 UPDATE_INTERVAL_SECOND=0.1
 
-# leds name and PIN, in this case I use a RGB combo LED, you can set to as many as your device supported
+# leds name and PIN, in this case I use a RGB combo LED, 
+# you can set as many as your device supported
 # the key-value of this dict is  name:pin# 
 LEDS = {
         'red':22,
@@ -34,10 +30,9 @@ LEDS = {
 
 class PwmLed:
     '''PWM LED class'''
-    def __init__(self, name, pin, freq, duty_cycle, r):
+    def __init__(self, r, freq, duty_cycle, name, pin):
+        # save redis connection, will be used later
         self.r = r
-        self.pin = pin
-
         # init redis key-value
         self.name = name
         self.key_freq = name+":freq"
@@ -46,21 +41,20 @@ class PwmLed:
         self.r.set(self.key_duty, duty_cycle)
         self.freq = float(self.r.get(self.key_freq))
         self.duty = float(self.r.get(self.key_duty))
-
         # start PWM LED with initail values
-        GPIO.setup(self.pin, GPIO.OUT)
-        self.pwm = GPIO.PWM(self.pin, self.freq)
+        GPIO.setup(pin, GPIO.OUT)
+        self.pwm = GPIO.PWM(pin, self.freq)
         self.pwm.ChangeDutyCycle(self.duty)
         self.pwm.start(self.duty)
 
     def update(self):
-        '''get freq and duty from redis by key, if changed, apply changes to pwm'''
+        '''get freq and duty from redis by key, apply changes to pwm if any change'''
         freq = float(self.r.get(self.key_freq))
         if freq != self.freq:
             print self.name, freq, self.freq
             self.pwm.ChangeFrequency(freq)
             self.freq = freq
-        # duty should be checked to avoid exception
+        # duty should be checked to ensure valid value range
         duty = float(self.r.get(self.key_duty))
         if duty>=0.0 and duty <= 100.0 and duty != self.duty:
             print self.name, duty, self.duty
@@ -71,12 +65,14 @@ class PwmLed:
         self.pwm.stop()
 
 if __name__=='__main__':
-    # make arguements for all leds to init PwmLeds 
-    r = redis.Redis(HOST, PORT)
-    ARGS = map(lambda x: list(x) + [FREQ, DUTY_CYCLE, r], LEDS.items())
-    pwmLeds = map(lambda x: PwmLed(*x), ARGS)
+    r = redis.Redis(HOST, PORT) # make a redis connection
+    # init a PWM LED set
+    pwmLeds = map(lambda x: PwmLed(r, FREQ, DUTY_CYCLE, *x), LEDS.items())
     # continuously update each pwm LED
-    while True:
-        for pl in pwmLeds:
-            pl.update()
-        time.sleep(UPDATE_INTERVAL_SECOND)
+    try:
+        while True:
+            time.sleep(UPDATE_INTERVAL_SECOND)
+            for pl in pwmLeds:
+                pl.update()
+    except:
+        map(lambda pl: pl.close(), pwmLeds)
